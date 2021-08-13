ce5363ce20f56ccbf32b2aa1ca3ac0c80d7531b7
moeb98 <55314997+moeb98@users.noreply.github.com>
2021-08-13 14:17:13 +0200

    Delete up.sh

--- prometheus/config/scripts/up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Hello world " $@
-timestamp=`date +"%Y-%m-%d"`
-echo ${timestamp}
-sed 's/hello/world/' /data/templates/2021-07-08-template-downtime.md > /data/content/issues/${timestamp}-$1.md
-cp /data/templates/2021-07-08-template-downtime.md /data/content/issues/
-
-
