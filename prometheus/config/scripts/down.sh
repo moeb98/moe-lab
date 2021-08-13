9253c8b11562443eff98e9116815609e852310a9
moeb98 <55314997+moeb98@users.noreply.github.com>
2021-08-13 14:16:15 +0200

    Delete down.sh

--- prometheus/config/scripts/down.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Hello world " $@
-timestamp=$(date +%F)
-echo ${timestamp}
-sed 's/hello/world/' ./cstate/templates/template-downtime.md > ./cstate/content/issues/${timestamp}-$1.md
-cp ./cstate/templates/2021-07-08-template-downtime.md ./cstate/content/issues/
-
-
